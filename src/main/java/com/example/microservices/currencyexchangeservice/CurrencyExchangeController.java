2849dbe4d5e57d6426cdcefe3ba52e76c8d051e5
Mohit Jain <mohitkjain@outlook.com>
2019-02-21 17:58:33 +0530

    Updated with Cloud Sleuth

--- src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Environment environment;
 	
@@ -21,6 +25,7 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}", exchangeValue);
 		return exchangeValue;
 		
 	}
